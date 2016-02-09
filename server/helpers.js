c34963fec5c7b4cd182da4f7c2eb20ba200ceb59
sca <smartinsantos@gmail.com>
2016-02-09 13:33:36 -0600

    working on workers routes

--- server/helpers.js
+++ server/helpers.js
@@ -45,7 +45,6 @@ helpers.adminCreate = function (attrs) {
   });
 };
 
-//Workers Helpers
 
 //General Helpers
 helpers.generateHash = function(password) {
@@ -59,27 +58,6 @@ helpers.validPassword = function(enteredPassword, passwordHash) {
 };
 
 //Properties Helpers
-
-helpers.getAllProperties = function(){
-  return Propertie.find({ }, function (err, doc) {
-    if (err){ 
-      console.log('error getting properties',err);
-      return err;
-    };
-    return doc;
-  });
-};
-
-helpers.findPropertieByName = function(name){
-  return Propertie.findOne({ 'name': name }, function (err, doc) {
-    if (err){ 
-      console.log('error finding propertie name',err);
-      return err;
-    };
-    return doc;
-  });
-};
-
 
 helpers.createPropertie = function (attrs) {
   console.log('Creating Propertie...')
@@ -105,18 +83,8 @@ helpers.createPropertie = function (attrs) {
 
 //Workers Helpers
 
-helpers.findWorkerByEmail = function(email){
-  return Worker.findOne({ 'email': email }, function (err, doc) {
-    if (err){ 
-      console.log('error finding worker email',err);
-      return err;
-    };
-    return doc;
-  });
-};
-
 helpers.createWorker = function (attrs) {
-  console.log('Creating Propertie...')
+  console.log('Creating Worker...')
   // Create this object incase attrs contains any extra data we dont want/need
   var workerAttrs = {
     first: attrs.first,
@@ -139,49 +107,12 @@ helpers.createWorker = function (attrs) {
 
 // Jobs Helpers
 
-helpers.getAllJobs = function(){
-  return Jobs.find({ }, function (err, doc) {
-    if (err){ 
-      console.log('error getting Jobs',err);
-      return err;
-    };
-    return doc;
-  });
-};
-
-// helpers.findJobByPropName = function(name){
-//   helpers.findPropertieByName(name)
-//   .then(function(prop){
-//     //looks up for the job with the property object...
-//     console.log('propertie to look up', prop);
-//   })
-//   .catch (function(err){
-//     return err
-//   });
-// };
-
-
-// helpers.findJobByWorkerEmail = function(email){
-//   helpers.findWorkerByEmail(email)
-//   .then(function(worker){
-  
-//   })
-//   .catch (function(err){
-//     return err
-//   });
-// };
-
-
 helpers.createJob = function (attrs) {
   console.log('Creating Job....');
-  var workerMail = attrs.worker;
-  var propertieName = attrs.propertie;
   
   var jobAttrs = {
-    // propertie : attrs.propertie,
     unit : attrs.unit,
     description : attrs.description,
-    // worker : attrs.worker,
     date_assigned : attrs.date_assigned,
     date_completed : attrs.date_completed,
     status : attrs.status,
@@ -192,52 +123,35 @@ helpers.createJob = function (attrs) {
     quote : attrs.quote,
   };
 
-  //TESTING
-  // helpers.findWorkerByEmail(workerMail)
-  // .then(function(worker){
-  //   console.log('Worker: ', worker);
-  //   workerId = worker._id;
-  // });
-
-  // helpers.findWorkerByEmail(propertieName)
-  // .then(function(propertie){
-  //   console.log('Propertie: ', propertie);
-  //   propertieId = propertie._id;
-  // });
-
-
-  var job = new Job(jobAttrs);
-  return job.save(function(err){
-    if (err){
-      console.log('failed to create new job');
-      return err;
-    }
-
-    return job;
+  var workerMail = attrs.worker;
+  var propertieName = attrs.propertie;
+  console.log(workerMail);
+  
+  helpers.findWorkerByEmail(workerMail)
+  .then(function(worker){
+    console.log('this is worker: ', worker);
+    jobAttrs.worker = worker._id;
   });
 
+  helpers.findPropertieByName(propertieName)
+  .then(function(propertie){
+    console.log('this is the propertie: ', propertie)
+    jobAttrs.propertie = propertie._id;
+    return;
+  })
+  .then(function(){
+    var job = new Job(jobAttrs);
+    return job.save(function(err){
+      if (err){
+        console.log('failed to create new job');
+        return err;
+      }
+      return job;
+    });    
+  })
 };
 
-//GILBERT
-// helpers.createJob = function (attrs) {
-//   db.collection('Job').insert({...})
-//   db.collection('Propertie').insert({...})
-//   db.collection('Worker').insert({...})
-// }
 
-// helpers.getJob = function (id) {
-//   return Promise.all([
-//     db.collection('Job').findOne({...}).exec(),
-//     db.collection('Propertie').findOne({...}).exec(),
-//     db.collection('Worker').findOne({...}).exec(),
-//   ])
-//   .then(function (results) {
-//     var job = results[0]
-//     job.propertie = results[1]
-//     job.worker = results[2]
-//     return  
-//   })
-// }
 
 // creating
 // helpers.adminCreate(toyData.admin);
@@ -254,10 +168,5 @@ helpers.createJob = function (attrs) {
 // .then(function(propertie){
 //   console.log('finding propertie....', propertie)
 // });
-
-
-
-
-
 
 module.exports = helpers;
--- server/routes/workersRouter.js
+++ server/routes/workersRouter.js
@@ -5,19 +5,45 @@ var auth = require('../auth.js');
 //Uses DB config and Schema
 var db = require('../lib/db.js');
 var Workers = require('../lib/models/workers.js');
+var helpers = require('../helpers.js');
 
 // Get all projects for a user
 router.get('/', auth.requireAuth, function (req, res) {
-  res.json({'success':true}); 
+  
+  Workers.find({}, function (err, doc) {
+    if (err){ 
+      console.log('error getting Job',err);
+      return err;
+    };
+    return doc;
+  })
+  .then(function(workers){
+    res.status(200).json(workers);
+  })
+  .catch(function(err){
+    res.status(401).json({'error':true});
+  });
 });
 
+
 router.get('/:workerId', auth.requireAuth, function (req, res) {
-  var workerId = req.params.workerId;
-  res.json({'success':true,workerId:workerId}); 
-    
+  Workers.findOne({'_id':req.params.workerId}, function(err,doc){
+    if(err){
+      console.log('error getting Job',err);
+      return err;
+    }
+    return doc;
+  })
+  .then(function(worker){
+    res.status(200).json(worker);
+  })
+  .catch(function(err){
+    res.status(401).json({'error':true});
+  })   
 });
 
 router.post('/', auth.requireAuth, function (req, res) {
+    
     res.json({'success':true}); 
 });
 
