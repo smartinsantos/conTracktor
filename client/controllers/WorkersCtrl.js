93bcbf0b5b49b753359e268b929f8c385163d115
sca <smartinsantos@gmail.com>
2016-02-12 15:51:06 -0600

    working on editing workers by id

--- client/controllers/WorkersCtrl.js
+++ client/controllers/WorkersCtrl.js
@@ -1,20 +1,7 @@
 app.controller('WorkersCtrl', ['$scope','Workers', function($scope, Workers) {
   
   console.log('WorkersCtrl Loaded....')
-
-  $scope.worker = {
-    first: '',
-    last: '',
-    email: '',
-    phone: '',
-    address: {
-      street: '',
-      street2:'',
-      city:'',
-      state:'',
-      zip: '',
-    }
-  };
+  $scope.worker = {};
 
   //We are going to save all the workers here on load
   $scope.workers = '';
@@ -24,20 +11,26 @@ app.controller('WorkersCtrl', ['$scope','Workers', function($scope, Workers) {
     .then(function(workers){
       $scope.workers = workers;
     })
-  }();
+  };
 
   $scope.createWorker = function () {
     var newWorker = $scope.worker;
     Workers.create(newWorker)
     .then(function(res){
+
       $('#workerModal').modal('toggle');
+      $scope.getWorkers();
     })
     .catch(function(err){
       console.log('error ocurred: ', err);
     });
   };
 
+  $scope.log = function(val){
+    console.log(val);
+  }
 
+  $scope.getWorkers();
 
 
 
